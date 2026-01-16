cb67ffe107f0cd08f8ed766f30d0a06c2312301a
AYUSHI DUBEY <ayushidubey2512@gmail.com>
2026-01-16 11:32:48 +0530

    fix layout by importing bootstrap locally

--- clg/src/main.jsx
+++ clg/src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/auth.jsx'
 import { CartProvider } from './context/cart.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
